f0af20a625ac2aa21ce1f258b67cdeac5b143434
Yaroslav-Borisov <yarpolar@gmail.com>
2024-01-06 15:01:31 +0300

    Добавлена страницы карточки товара

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,9 +1,20 @@
-import { Await, useLoaderData } from 'react-router-dom';
+import { Await, useLoaderData, useParams } from 'react-router-dom';
 import { Product } from '../../interfaces/product.interface';
 import { Suspense } from 'react';
+import style from './Product.module.css';
+import { useDispatch } from 'react-redux';
+import { AppDispath } from '../../store/store';
+import { cartActions } from '../../store/cart.slice';
 
 export function Product() {
+	const dispatch = useDispatch<AppDispath>();
 	const data = useLoaderData() as { data: Product };
+	const {id} = useParams();
+
+	const add = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		e.preventDefault();
+		dispatch(cartActions.add(Number(id)));
+	};
 
 	return <>
 		<Suspense fallback={'Загружаю...'}>
@@ -11,7 +22,15 @@ export function Product() {
 				resolve={data.data}
 			>
 				{({ data }: { data: Product }) => (
-					<>Product - {data.name}</>
+					<div className={style['wrapper']}>
+						<h1 className={style['title']}>«{data.name}»</h1>
+						<img className={style['image']} src={data.image}/>
+						<div className={style['price']}>Стоимость: {data.price} рублей</div>
+						<ul className={style['list']}>
+							{data.ingredients.map((ingridient, index) => <li className={style['item']} key={index}>{ingridient}</li>)}
+						</ul>
+						<button className={style['button']} onClick={add}>Добавить в корзину</button>
+					</div>
 				)}
 			</Await>
 		</Suspense>
